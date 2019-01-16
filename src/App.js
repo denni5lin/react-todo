3ab8043dc5a35294164cad51b74cc23e413b6049
Dennis Lin <lin.dennis18@gmail.com>
2019-01-15 20:01:42 -0800

    'Load' data in to the component's state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import TodoItem from "./TodoItem";
 import todosData from "./todosData";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+  }
   render() {
-    const todoList = todosData.map(item => (
+    const todoList = this.state.todos.map(item => (
       <TodoItem key={item.key} item={item} />
     ));
     return <div className="todo-list">{todoList}</div>;
