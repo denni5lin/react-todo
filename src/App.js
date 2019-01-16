28cd49088592158f292b3cd4c5bea15a982c1cd4
Dennis Lin <lin.dennis18@gmail.com>
2019-01-15 20:21:33 -0800

    onChange events

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import todosData from "./todosData";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       todos: todosData
-    }
+    };
   }
   render() {
     const todoList = this.state.todos.map(item => (
       <TodoItem key={item.key} item={item} />
     ));
+
     return <div className="todo-list">{todoList}</div>;
   }
 }
